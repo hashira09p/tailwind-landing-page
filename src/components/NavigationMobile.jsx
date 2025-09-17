dba2a15cb7c82c8e6549c1eeb352f2aef4857b84
Je Rome <jeromeabelida19@gmail.com>
2025-09-18 00:08:22 +0800

    Just finished the navigation for mobile. Task tommorrow: create a hero section

--- src/components/NavigationMobile.jsx
+++ src/components/NavigationMobile.jsx
@@ -3,21 +3,33 @@ import NavigationDesktop from "./NavigationDesktop";
 import { useState } from "react";
 
 function toggleMenu() {
+
   return (
-    <div className="w-full h-[300px] bg-black flex flex-col gap-2">
-      <div className="flex-1 p-2 text-lg text-neon-green font-medium font-roboto inline-block content-center">
+    <div className="w-full h-[300px] bg-black flex flex-col gap-2 text-neon-green text-lg font-medium font-roboto">
+
+      <div className="flex-1 p-2 content-center">
         <div className="w-fit h-auto m-auto">
-          <a href="">Home</a>
+          <a href="" className="transition-all duration-300 ease-in-out hover:text-green-400 hover:text-shadow-[0_0_10px_#00ff00]">Home</a>
         </div>
       </div>
-      <div className="flex-1 p-2">
-        <a href="">Skill</a>
+
+      <div className="flex-1 p-2  content-center">
+        <div className="w-fit h-auto m-auto">
+          <a href="" className="transition-all duration-300 ease-in-out hover:text-green-400 hover:text-shadow-[0_0_10px_#00ff00]">Skills</a>
+        </div>
       </div>
-      <div className="flex-1 p-2 ">
-        <a href="">Projects</a>
+
+      <div className="flex-1 p-2 content-center ">
+        <div className="w-fit h-auto m-auto">
+          <a href="" className="transition-all duration-300 ease-in-out hover:text-green-400 hover:text-shadow-[0_0_10px_#00ff00]">Projects</a>
+        </div>
+
       </div>
+
       <div className="flex-1 p-2 ">
-        <a href="">Contact</a>
+        <div className="w-fit h-auto m-auto">
+          <a href="" className="transition-all duration-300 ease-in-out hover:text-green-400 hover:text-shadow-[0_0_10px_#00ff00]">Contact</a>
+        </div>
       </div>
     </div>
   )
@@ -28,29 +40,36 @@ const NavigationMobile = () => {
 
   return (
     <div className="fixed inset-0 z-20 w-full backdrop-blur-lg">
-      <nav className="w-full h-[70px] bg-black flex">
-        <div className="w-[50%] h-full p-2">
-          <img src="neonByte.png" alt="" className="w-[60px] h-full" />
-        </div>
-        <div className="w-[50%] h-full p-2 relative">
-          <svg
-            width="52px"
-            height="52px"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            id="menu-alt"
-            className="icon glyph trasition-all duration-300 ease-in-out cursor-pointer hover:color-green-400 absolute right-[10px]"
-            fill="#1FA164"
-            stroke="#1FA164"
-          >
-            <path d="M21,19H3a1,1,0,0,1,0-2H21a1,1,0,0,1,0,2Z" style={{ fill: "#1FA164" }} />
-            <path d="M21,13H3a1,1,0,0,1,0-2H21a1,1,0,0,1,0,2Z" style={{ fill: "#1FA164" }} />
-            <path d="M21,7H3A1,1,0,0,1,3,5H21a1,1,0,0,1,0,2Z" style={{ fill: "#1FA164" }} />
-          </svg>
-        </div>
-      </nav>
+      <div>
+
+        <nav className="w-full h-[70px] bg-black flex">
+          <div className="w-[50%] h-full p-2">
+            <img src="neonByte.png" alt="" className="w-[60px] h-full transition-all duration-500 ease-in-out hover:shadow-[0_0_10px_#00ff00]" />
+          </div>
+          <div className="w-[50%] h-full p-2 relative">
+            <button className="w-fit h-fit absolute right-[10px]" onClick={() => setIsOpen(true)}>
+              <svg
+                className="icon glyph trasition-all duration-300 ease-in-out cursor-pointer hover:shadow-[0_0_10px_#00ff00]"
+                width="52px"
+                height="52px"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                id="menu-alt"
+                fill="#1FA164"
+                stroke="#1FA164"
+              >
+                <path d="M21,19H3a1,1,0,0,1,0-2H21a1,1,0,0,1,0,2Z" style={{ fill: "#1FA164" }} />
+                <path d="M21,13H3a1,1,0,0,1,0-2H21a1,1,0,0,1,0,2Z" style={{ fill: "#1FA164" }} />
+                <path d="M21,7H3A1,1,0,0,1,3,5H21a1,1,0,0,1,0,2Z" style={{ fill: "#1FA164" }} />
+              </svg>
+            </button>
+          </div>
+        </nav>
+        {isOpen && toggleMenu()}
+      </div>
+
     </div>
   );
 };
 
-export default toggleMenu;
+export default NavigationMobile;
